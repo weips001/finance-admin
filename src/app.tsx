c997def2a2a4085f5dd494c9a4ae536e7402d703
pengshuai.wei <caa-it-bda@mail.foxconn.com>
2021-06-19 16:30:29 +0800

    修改app.ts

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 // import { createLogger } from 'redux-logger';
-import { message, notification } from 'antd';
+import { notification } from 'antd';
 
 export const dva = {
   config: {
     // onAction: createLogger(),
     onError(e: Error) {
       console.log(e)
-      const {msg = '位置错误'} = e
+      const { msg = '位置错误' } = e
       // message.error(e, 3) ;
       notification.error({
         message: `操作失败`,
